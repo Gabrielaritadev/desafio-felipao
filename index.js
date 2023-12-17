6a6ec3b3aafe85d38c3d919feca4cd8742d953f0
Gabrielaritadev <gabrielarita1995@gmail.com>
2023-12-17 14:49:34 -0300

    alterações

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-let nome
-let nivelXp
-
-
-nome = ("Bem Vindo, digite o seu nome para iniciar")
-
-
-nivelXp = ("Digite o nível de XP:");
-
+let nome = "Bruna "
+let nivelXp = 0
 
 if (nivelXp >= 0 && nivelXp <1000){
-  console.log("Voce esta no nivel FERRO")
+  console.log(nome + "Voce esta no nivel FERRO") 
 }
 else if (nivelXp > 1000 && nivelXp <= 2000){
-console.log ("Voce está no nível BRONZE")
+  console.log (nome + "Voce está no nível BRONZE")
 }
 else if (nivelXp >2001 && nivelXp <=5000){
-    console.log ("Voce está no nível Prata Ouro")
+    console.log (nome + "Voce está no nível Prata Ouro")
 }
 else if (nivelXp >5001 && nivelXp <= 8000){
-    console.log ("Voce está no nível Platina Diamante")
+    console.log (nome + "Voce está no nível Platina Diamante")
 }
 else if (nivelXp >8001 && nivelXp <= 9000){
-    console.log("Voce está no nível Ascendente")
-}else{(nivelXp > 9000)
-    console.log ("Parabéns você atingiu o nivel maximo e por isso foi escolhido para ser o rei")}
+    console.log(nome + "Voce está no nível Ascendente")
+}
+else{(nivelXp > 9000) 
+    console.log ("Parabéns " + nome + "você atingiu o nivel maximo e por isso foi escolhido(a) para ser o rei")}
+ 
+
 
