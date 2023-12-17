316545a9aa1e6cd2d3a8e120ffb67604511b6a37
Gabrielaritadev <gabrielarita1995@gmail.com>
2023-12-17 16:13:49 -0300

    alteraçõesfinais

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 let nome = "Gustavo "
-let nivelXp = 2.000
+let quantidadeXp = 1.000
 
-if (nivelXp <=1.000){
+if (quantidadeXp <=1.000){
   console.log("O herói " + nome + "está no nivel de FERRO") 
 }
-else if (nivelXp > 1.000 && nivelXp <= 2.000){
+else if (quantidadeXp > 1.000 && quantidadeXp <= 2.000){
   console.log ("O herói " + nome + "está no nivel de BRONZE")
 }
-else if (nivelXp >2.001 && nivelXp <=5.000){
-    console.log ("O herói " + nome + "está no nivel de Prata")
+else if (quantidadeXp >2.001 && quantidadeXp <=5.000){
+    console.log ("O herói " + nome + "está no nivel de PRATA")
 }
-else if (nivelXp >5.001 && nivelXp <= 8.000){
-    console.log ("O herói " + nome + "está no nivel de Ouro")
+else if (quantidadeXp >5.001 && quantidadeXp <= 8.000){
+    console.log ("O herói " + nome + "está no nivel de OURO")
 }
-else if (nivelXp >8.001 && nivelXp <= 9.000){
-    console.log("O herói " + nome + "está no nivel de Platina")
+else if (quantidadeXp >8.001 && quantidadeXp <= 9.000){
+    console.log("O herói " + nome + "está no nivel de PLATINA")
 }
-else if (nivelXp >9.001 && nivelXp <= 10.000){
-  console.log("O herói " + nome + "está no nivel de Imortal")
+else if (quantidadeXp >9.001 && quantidadeXp <= 10.000){
+  console.log("O herói " + nome + "está no nivel de IMORTAL")
 }
-else if (nivelXp >= 10.001){
-    console.log("O herói " + nome + "está no nivel de Radiante")
+else if (quantidadeXp >= 10.001){
+    console.log("O herói " + nome + "está no nivel de RADIANTE")
 }
-
 
